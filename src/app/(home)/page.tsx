436d86cedcfd593086153541dd0c898f559cc9fd
Ogulcan <account.work@ogulcan.dev>
2023-10-07 19:04:04 +0300

    build trigger

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ import ExampleServerComponent from "@/components/organisms/exampleServerComponen
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <span className="">Home</span>
+      <span className="">ogulcan</span>
       <ExampleClientComponent />
       <ExampleServerComponent />
     </main>
